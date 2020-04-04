a43e7f5001a7a647ceaf3ba6005ab6ddf170619f
nezar <nezardumairieh@gmail.com>
2020-04-04 13:58:20 -0800

    first commit

---
+++ gift1/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace gift1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            BunifuAnimatorNS.Animation animation1 = new BunifuAnimatorNS.Animation();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linerrockt = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rocket = new System.Windows.Forms.PictureBox();
+            this.anim = new BunifuAnimatorNS.BunifuTransition(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linerrockt
+            // 
+            this.linerrockt.BackColor = System.Drawing.Color.Red;
+            this.anim.SetDecoration(this.linerrockt, BunifuAnimatorNS.DecorationType.None);
+            this.linerrockt.ForeColor = System.Drawing.Color.Blue;
+            this.linerrockt.Location = new System.Drawing.Point(11, 0);
+            this.linerrockt.Name = "linerrockt";
+            this.linerrockt.Size = new System.Drawing.Size(1377, 10);
+            this.linerrockt.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.anim.SetDecoration(this.label1, BunifuAnimatorNS.DecorationType.None);
+            this.label1.Font = new System.Drawing.Font("Andalus", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(521, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 144);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "  فريق الإبداع البرمجي\r\nEn.Nezar Dumairieh\r\n     05550167003";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.anim.SetDecoration(this.label2, BunifuAnimatorNS.DecorationType.None);
+            this.label2.Font = new System.Drawing.Font("Andalus", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(526, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 171);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "      العيادة السنية  \r\nالدكتور :خالد عبد الرحمن\r\n    +973214325678";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.linerrockt);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.anim.SetDecoration(this.panel1, BunifuAnimatorNS.DecorationType.None);
+            this.panel1.Location = new System.Drawing.Point(7, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1377, 198);
+            this.panel1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.anim.SetDecoration(this.label3, BunifuAnimatorNS.DecorationType.None);
+            this.label3.Font = new System.Drawing.Font("Andalus", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(504, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(352, 171);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "  فريق الإبداع البرمجي\r\nEn.Nezar Dumairieh\r\n  05550167003";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox3
+            // 
+            this.anim.SetDecoration(this.pictureBox3, BunifuAnimatorNS.DecorationType.None);
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(4, 683);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(122, 92);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.anim.SetDecoration(this.label4, BunifuAnimatorNS.DecorationType.None);
+            this.label4.Font = new System.Drawing.Font("Andalus", 24F);
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(318, 403);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 48);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Login  Page\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.anim.SetDecoration(this.pictureBox1, BunifuAnimatorNS.DecorationType.None);
+            this.pictureBox1.Location = new System.Drawing.Point(621, 277);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rocket
+            // 
+            this.rocket.BackColor = System.Drawing.Color.Transparent;
+            this.anim.SetDecoration(this.rocket, BunifuAnimatorNS.DecorationType.None);
+            this.rocket.Location = new System.Drawing.Point(621, 277);
+            this.rocket.Name = "rocket";
+            this.rocket.Size = new System.Drawing.Size(197, 144);
+            this.rocket.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rocket.TabIndex = 15;
+            this.rocket.TabStop = false;
+            // 
+            // anim
+            // 
+            this.anim.AnimationType = BunifuAnimatorNS.AnimationType.Transparent;
+            this.anim.Cursor = null;
+            animation1.AnimateOnlyDifferences = true;
+            animation1.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.BlindCoeff")));
+            animation1.LeafCoeff = 0F;
+            animation1.MaxTime = 1F;
+            animation1.MinTime = 0F;
+            animation1.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicCoeff")));
+            animation1.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicShift")));
+            animation1.MosaicSize = 0;
+            animation1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            animation1.RotateCoeff = 0F;
+            animation1.RotateLimit = 0F;
+            animation1.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.ScaleCoeff")));
+            animation1.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.SlideCoeff")));
+            animation1.TimeCoeff = 0F;
+            animation1.TransparencyCoeff = 1F;
+            this.anim.DefaultAnimation = animation1;
+            // 
+            // pictureBox2
+            // 
+            this.anim.SetDecoration(this.pictureBox2, BunifuAnimatorNS.DecorationType.None);
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1312, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 51);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 775);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rocket);
+            this.anim.SetDecoration(this, BunifuAnimatorNS.DecorationType.None);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rocket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel linerrockt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox rocket;
+        private BunifuAnimatorNS.BunifuTransition anim;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ gift1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gift1
+{
+    public partial class Form1 : Form
+    {
+        bool d = false;
+        Point start_position = new Point(0, 0);
+
+        Timer tback = new Timer();
+        Timer trocket = new Timer();
+        Timer tball = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            BackColor = Color.FromArgb(253, 253, 253);
+            tback.Tick += new EventHandler(re);
+            tball.Tick += new EventHandler(bal);
+            trocket.Tick += new EventHandler(roc);
+            def();
+
+        }
+
+        void def()
+        {
+            linerrockt.Height = 7;
+            linerrockt.Width = 910;
+            linerrockt.Left = 20;//location 307, 275
+
+            //picture 1
+
+
+            label2.Top = 10;//location 399, 234
+            label2.Visible = false;
+            label2.BringToFront();
+
+
+
+            pictureBox1.Top = 600;//location 399, 234
+            pictureBox1.Visible = false;
+            pictureBox1.BringToFront();
+
+            //picture 2 49, -3 size =100
+            rocket.Height = 400;
+            rocket.Top = 600;
+            rocket.Visible = false;
+            pictureBox1.ImageLocation = @"C:\Users\nezar\source\repos\gift1\gift1\images\th2DNELSZX.jpg";
+            rocket.ImageLocation = @"C:\Users\nezar\source\repos\gift1\gift1\images\th2DNELSZX.jpg";
+            anim.HideSync(label1);
+
+
+            //timer interval
+            //timer interval
+            tback.Interval = 10;
+            tball.Interval = 20;
+            trocket.Interval = 30;
+            tback.Start();
+
+        }
+        //initial
+        int wd = 410;
+        void re(object sender, EventArgs e)
+        {
+            linerrockt.Width += wd;
+            if (linerrockt.Width < 10)
+            {
+                wd = 10;
+            }
+            if (linerrockt.Width > 50)
+            {
+                tback.Stop();
+                trocket.Start();
+            }
+            anim.ShowSync(label2);
+        }
+
+        int dawn = 1, lt = 5, rt = -1;
+        void bal(object sender, EventArgs e)
+        {
+            pictureBox1.Top += dawn;
+            label2.Top += dawn;
+            if (pictureBox1.Top < 100)
+            {
+                pictureBox1.Visible = true;
+                label2.Visible = true;
+
+                dawn = 325;
+            }
+            if (pictureBox1.Top > 510)
+            {
+                tball.Interval = 170;
+                rocket.ImageLocation = @"C:\Users\nezar\source\repos\gift1\gift1\images\5.jpg";
+                rocket.ImageLocation = @"C:\Users\nezar\source\repos\gift1\gift1\images\5.jpg";
+                pictureBox1.Visible = false;
+                label1.Visible = false;
+                dawn = 5;
+                rocket.Top -= dawn;
+                if (rocket.Top < -95)
+                {
+                    linerrockt.Width -= lt;
+                    linerrockt.Left += lt;
+                    if (linerrockt.Left > 310)
+                    {
+                        dawn = 1;
+                        tball.Stop();
+                        //def();
+                        this.Hide();
+                        Form2 f1 = new Form2();
+
+                        f1.ShowDialog();
+
+
+                      
+
+
+
+                    }
+                }
+
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void roc(object sender, EventArgs e)
+        {
+            rocket.Top += rt;
+            if (rocket.Top > 280)
+            {
+                rocket.Visible = true;
+                rt = -1;
+            }
+            if (rocket.Top < 170)
+            {
+                rt = -7;
+                trocket.Stop();
+                tball.Start();
+            }
+        }
+
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
